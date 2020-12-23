b0eebd97898c2ab8b1a96bb2d16f0d919cb9dbb1
walle <975965715@qq.com>
2020-12-23 23:20:26 +0800

    chore: update dependency;

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './logical/user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    MongooseModule.forRoot('mongodb://localhost/blog'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
