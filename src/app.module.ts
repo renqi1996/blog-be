ba1c60a3188cc0066f67badabd3e2573f793fbd9
walle <975965715@qq.com>
2020-12-29 23:25:59 +0800

    feat: update dependency; init jwt file;

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './logical/user/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthService } from './logical/auth/auth.service';
+import { AuthModule } from './logical/auth/auth.module';
 
 @Module({
   imports: [
     UserModule,
     MongooseModule.forRoot('mongodb://localhost/blog'),
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
---
+++ src/logical/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+  secret: 'waller8719', // secret key
+}
